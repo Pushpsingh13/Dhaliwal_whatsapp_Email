7f3e25634ea92cfda3adb46a0a279b3ae02cf4fa
Pushpsingh13 <Pushp.singh13@gmail.com>
2025-11-26 21:23:07 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -1015,7 +1015,7 @@ import os
 OWNER_EMAIL = st.secrets["OWNER_EMAIL"]
 SENDER_EMAIL = st.secrets["SENDER_EMAIL"]
 SENDER_PASSWORD = st.secrets["SENDER_PASSWORD"]
-SEND_TIME = st.secrets.get("SEND_TIME", "21:15")  # 20:50 PM default
+SEND_TIME = st.secrets.get("SEND_TIME", "23:59")  # 23:59 PM default
 
 
 # ==============================
@@ -1094,6 +1094,7 @@ st.markdown(
 )
 
 st.empty()
+
 
 
 
