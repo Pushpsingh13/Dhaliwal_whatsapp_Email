26073ce7b3b8656d9278a384730e42eef3be0216
Pushpsingh13 <Pushp.singh13@gmail.com>
2025-11-28 11:39:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ with st.container():
         st.image(LOGO_PATH, width=100)
     with col2:
         st.image(QR_CODE_APP_PATH, width=100)
-                st.write("Timimg 11:00 am to 11:00 Pm. Please click on the Button for Download the App for next order. Minimum order for delivery should be 300₹ ")
+        st.write("Timimg 11:00 am to 11:00 Pm. Please click on the Button for Download the App for next order. Minimum order for delivery should be 300₹")
                   # --- DOWNLOAD APP BUTTON ---
         st.markdown(
             """
@@ -81,7 +81,7 @@ with st.container():
             """,
             unsafe_allow_html=True
         )
-with col3:
+    with col3:
         st.empty()
 # =========================
 # PAGE CONFIG & STYLING
@@ -1114,6 +1114,7 @@ else:
 
 # optional: show the last run date
 st.write("Last run date (server file):", last_run_date)
+
 
 
 
