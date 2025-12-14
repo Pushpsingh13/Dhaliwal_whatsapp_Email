518a65022c0bc92cfebf37ea5e031c557dfb4f6a
Pushpsingh13 <Pushp.singh13@gmail.com>
2025-12-15 00:25:05 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -66,10 +66,7 @@ with st.container():
         st.image(LOGO_PATH, width=100)
     with col2:
         st.image(QR_CODE_APP_PATH, width=100)
-        st.write("Dhaliwal’s Food Court
-Operating Hours: 10:00 AM – 10:00 PM
-Click the button below to download the app and place your next order.
-Pickup only (as per order).")
+        st.write("Dhaliwal’s Food Court Operating Hours: 10:00 AM – 10:00 PM Click the button below to download the app and place your next order. Pickup only (as per order).")
                   # --- DOWNLOAD APP BUTTON ---
         st.markdown(
             """
@@ -1190,4 +1187,5 @@ st.markdown("[Cancellation & Refunds](https://merchant.razorpay.com/policy/Rfv4u
 
 with st.expander("Privacy Policy - Dhaliwals Food Court Unit of Param Mehar Enterprise Prop Pushpinder Singh Dhaliwal"):
     privacy_policy_component("privacy_policy.html")
+
 
