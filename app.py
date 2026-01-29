1b740a63b3e98336a2abe29a89dd2f135e265a37
Pushpsingh13 <Pushp.singh13@gmail.com>
2026-01-29 16:56:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ with c1:
 with c2:
     st.markdown("<h1>Dhaliwals Food Court</h1>", unsafe_allow_html=True)
     st.markdown("Unit of Param Mehar Enterprise Prop Pushpinder Singh Dhaliwal")
-    st.markdown("***Timming 🕒 10:00 AM – 10:00 PM • For any enquiry📞 +91-9259317713*** \n\n  **-"Pickup Only • Freshly Prepared Orders required time as per order item._**")
+    st.markdown("***Timming 🕒 10:00 AM – 10:00 PM For any enquiry📞 +91-9259317713*** \n\n  **-"Pickup Only Freshly Prepared Orders required time as per order item._**")
     
     # Download Button and Review Link (Stacked)
     st.markdown(
@@ -1298,5 +1298,6 @@ st.markdown("[Cancellation & Refunds](https://merchant.razorpay.com/policy/Rfv4u
 
 with st.expander("Privacy Policy - Dhaliwals Food Court Unit of Param Mehar Enterprise Prop Pushpinder Singh Dhaliwal"):
     privacy_policy_component("privacy_policy.html")
+
 
 
